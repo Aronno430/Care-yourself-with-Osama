a45846e3a968f1b76b9f62cb6fabe33e3393e16e
Aronnoparvez <aronnoparvez6@gmail.com>
2025-04-30 09:13:46 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
-let cart = [];
-
-function addToCart(product) {
-  cart.push(product);
-  alert(product + " added to cart!");
-  localStorage.setItem("cart", JSON.stringify(cart));
-}
-
-function loadCart() {
-  const cartItems = document.getElementById("cart-items");
-  const storedCart = JSON.parse(localStorage.getItem("cart")) || [];
-  cartItems.innerHTML = storedCart.map(item => `<li>${item}</li>`).join('');
-}
-
-function checkout() {
-  alert("Order placed!");
-  localStorage.removeItem("cart");
-  document.getElementById("cart-items").innerHTML = "";
-}
-
-window.onload = () => {
-  if (document.getElementById("cart-items")) {
-    loadCart();
-  }
-};
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for your message! Osama will reach out soon.");
+});
